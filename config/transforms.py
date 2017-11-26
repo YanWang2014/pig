1eb45330999501804e6080f35d59498b1ad38469
Yan Wang <w-y10@foxmail.com>
2017-11-26 14:29:33 -0500

    add new scale and pad2set

--- config/transforms.py
+++ config/transforms.py
@@ -23,11 +23,12 @@ from torchvision import transforms
 import random
 from PIL import Image
 from .transforms_master import ColorJitter, scale, ten_crop, to_tensor
+import collections
 
 #input_size = 224 
 #train_scale = 256 
 #test_scale = 256
-normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) # rerange them to [-1, +1]
 
 def my_transform(img, input_size, train_scale, test_scale):
     img = scale(img, test_scale)
@@ -60,6 +61,75 @@ def my_transform_multiscale_test(varied_scale, flip_flag):
         transforms.ToTensor(),
         normalize
     ])
+
+#https://jdhao.github.io/2017/11/06/resize-image-to-square-with-padding/#more
+            transforms.Scale_larger(test_scale),   # 将长边scale到test_scale，保持长宽比
+        transforms.Pad2Square(input_size)
+def my_resize(img, size, interpolation=Image.BILINEAR):
+    """ Adapted from but opposite to the oficial resize function, i.e.
+        If size is an int, the larger edge of the image will be matched to this number maintaing
+            the aspect ratio. i.e, if height > width, then image will be rescaled to
+            (size, size * width / height)
+    """
+    if not _is_pil_image(img):
+        raise TypeError('img should be PIL Image. Got {}'.format(type(img)))
+    if not (isinstance(size, int) or (isinstance(size, collections.Iterable) and len(size) == 2)):
+        raise TypeError('Got inappropriate size arg: {}'.format(size))
+
+    if isinstance(size, int):
+        w, h = img.size
+        if (w >= h and w == size) or (h >= w and h == size):
+            return img
+        if w > h:
+            ow = size
+            oh = int(size * h / w)
+            return img.resize((ow, oh), interpolation)
+        else:
+            oh = size
+            ow = int(size * w / h)
+            return img.resize((ow, oh), interpolation)
+    else:
+        return img.resize(size[::-1], interpolation)
+class my_Resize(object):
+    def __init__(self, size, interpolation=Image.BILINEAR):
+        assert isinstance(size, int) or (isinstance(size, collections.Iterable) and len(size) == 2)
+        self.size = size
+        self.interpolation = interpolation
+    def __call__(self, img):
+        return my_resize(img, self.size, self.interpolation)
+    
+class Pad2Set(object):
+    """ Adapted from but different to the oficial Pad class, i.e.
+    Pad the given PIL Image on all sides to the target value while keeping the original image
+    in the center.
+
+    Args:
+        padding (int or tuple): Padding on each border. If a single int is provided this
+            is used to pad all borders. If tuple of length 2 is provided this is the padding
+            on w and h respectively. 
+        fill: Pixel fill value. Default is 0. If a tuple of
+            length 3, it is used to fill R, G, B channels respectively.
+    """
+
+    def __init__(self, padding, fill=0):
+        assert isinstance(padding, (numbers.Number, tuple))
+        assert isinstance(fill, (numbers.Number, str, tuple))
+        if isinstance(padding, collections.Sequence) and len(padding) not in [2]:
+            raise ValueError("Padding must be an int or a 2 element tuple, not a " +
+                             "{} element tuple".format(len(padding)))
+        self.padding = padding
+        self.fill = fill
+    def __call__(self, img):
+        #own code here, note that self.padding refers to the target size first, and
+        #then goes back to its original meaning defined in the official pad class or function.
+        #i.e. if a tuple of length 4 is provided
+        #    this is the padding for the left, top, right and bottom borders respectively.
+        w, h = img.size
+        if len(self.padding) > 1:
+            raise ValueError("Currently Pad2Set only support input of a single int")
+        self.padding = ((self.padding-w)//2, (self.padding-h)//2, 
+                        self.padding-(self.padding-w)//2, self.padding-(self.padding-h)//2)
+        return pad(img, self.padding, self.fill)
 
 composed_data_transforms = {}
 def data_transforms(phase, input_size = 224, train_scale = 256, test_scale = 256):
@@ -99,6 +169,12 @@ def data_transforms(phase, input_size = 224, train_scale = 256, test_scale = 256
         transforms.ToTensor(),
         normalize
     ]),
-    'ten_crop': my_ten_crops(input_size, train_scale, test_scale)#todo: merge my_transform
+    'ten_crop': my_ten_crops(input_size, train_scale, test_scale),#todo: merge my_transform
+    'scale_pad': transforms.Compose([ 
+        transforms.my_Resize(test_scale),   # 将长边scale到test_scale，保持长宽比
+        transforms.Pad2Set(input_size), # pad 成正方形，边长为input_size
+        transforms.ToTensor(),
+        normalize
+    ]),
     }
     return composed_data_transforms[phase]
