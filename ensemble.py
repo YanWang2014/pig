6fb257c8f34e300961ce7ac41fcd7805ed667183
Yan Wang <w-y10@foxmail.com>
2017-12-03 11:18:04 -0500

    faster ensemble based on pd pivot table

--- ensemble.py
+++ ensemble.py
@@ -13,16 +13,23 @@ dfs = []
 for file in files:
     dfs.append(pd.read_csv(file, header = None))
 
-temps = [df.groupby([0,1])[2].sum() for df in dfs]
-means = sum(temps)/len(temps)
+#temps = [df.groupby([0,1])[2].sum() for df in dfs]
+#means = sum(temps)/len(temps)
+
+#df = pd.DataFrame(columns=(0, 1, 2))
+#for i in range(len(means)):
+#    if i%1000 == 0:
+#        print (i)
+#    df.loc[i] = [means.index[i][0], means.index[i][1], means[means.index[i][0], means.index[i][1]]]
+#
+#    
+
+pivots = []
+for i in range(len(dfs)):
+    pivots.append(pd.pivot_table(dfs[i], index=[0,1]))
+
+mean_pivot = sum(pivots)/len(pivots)
 
 
-
-df = pd.DataFrame(columns=(0, 1, 2))
-for i in range(len(means)):
-    if i%1000 == 0:
-        print (i)
-    df.loc[i] = [means.index[i][0], means.index[i][1], means[means.index[i][0], means.index[i][1]]]
-
-    
+df = mean_pivot.reset_index()  
 df.to_csv('result/me2/ensemble.csv', header = None, index = None)
